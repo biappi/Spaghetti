acc1a56b9e785762a78f402e205d7d137c8dab96
Antonio Malara <amalara@ebuddy.com>
2013-01-07 14:13:53 +0100

    Initial Commit

---
+++ SpaghettiTests/SpaghettiTests.h
@@ -0,0 +1,13 @@
+//
+//  SpaghettiTests.h
+//  SpaghettiTests
+//
+//  Created by Antonio "Willy" Malara on 1/7/13.
+//  Copyright (c) 2013 Antonio "Willy" Malara. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SpaghettiTests : SenTestCase
+
+@end
---
+++ SpaghettiTests/SpaghettiTests.m
@@ -0,0 +1,32 @@
+//
+//  SpaghettiTests.m
+//  SpaghettiTests
+//
+//  Created by Antonio "Willy" Malara on 1/7/13.
+//  Copyright (c) 2013 Antonio "Willy" Malara. All rights reserved.
+//
+
+#import "SpaghettiTests.h"
+
+@implementation SpaghettiTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SpaghettiTests");
+}
+
+@end
