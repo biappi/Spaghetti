32eb8657c1a7fdf75edff7bb40b9c3bb11ad3c69
Antonio Malara <antonio.malara@gmail.com>
2013-01-09 15:38:22 +0100

    Added first round of testing

--- SpaghettiTests/SpaghettiTests.m
+++ SpaghettiTests/SpaghettiTests.m
@@ -7,6 +7,7 @@
 //
 
 #import "SpaghettiTests.h"
+#import "SGMachine.h"
 
 @implementation SpaghettiTests
 
@@ -18,8 +19,25 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    STAssertTrue(YES, @"Always passing");
+- (void)testCreation {
+    SGMachine * fsm;
+    
+    fsm = [[SGMachine alloc] initWithInitialState:@"a"];
+    STAssertEqualObjects(@"a", fsm.state, @"State is not the initial state");
+    STAssertEqualObjects(@"digraph {\n\n}\n", [fsm graphvizString], @"Graph should be empty");
+    
+    [fsm onEvent:@"goto_b" duringState:@"a" transitionToState:@"b"];
+    STAssertEqualObjects(@"digraph {\n  \"a\" -> \"b\" [label=\"goto_b\"];\n\n}\n",
+                         [fsm graphvizString],
+                         @"Graph serialization do not match");
+
+    [fsm onEvent:@"goto_a" duringState:@"b" transitionToState:@"a"];
+    STAssertEqualObjects(@"digraph {\n"
+                         "  \"a\" -> \"b\" [label=\"goto_b\"];\n"
+                         "  \"b\" -> \"a\" [label=\"goto_a\"];\n"
+                         "\n}\n",
+                         [fsm graphvizString],
+                         @"Graph serialization do not match");
 }
 
 @end
